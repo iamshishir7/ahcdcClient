efc96af927977c819414cc5e405b9a81a030359f
Shishir Timalsina <62695104+iamshishir7@users.noreply.github.com>
2021-01-18 23:04:18 +1100

    fix8

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,6 +1,7 @@
 
 
-export default {
+
+const AuthService= {
     login : user =>{
         console.log(user);
         return fetch('/user/login',{
@@ -34,12 +35,7 @@ export default {
     },
     isAuthenticated : ()=>{
         
-        return fetch('/user/authenticated', {
-            method : "get",
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        })
+        return fetch('/user/authenticated')
                 .then(res=>{
                     if(res.status !== 401)
                         return res.json().then(data => data);
@@ -49,4 +45,6 @@ export default {
     }
 
 }
+
+export default AuthService
 
